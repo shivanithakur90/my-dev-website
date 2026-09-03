ea443d9916ff31b556f4f24a60f7fd1def7bf3d4
Sunny Thakur <sunnythakursunny650@gmail.com>
2026-09-04 01:51:22 +0530

    Show work cards in two mobile columns

--- components/home/WorkedOnSection.tsx
+++ components/home/WorkedOnSection.tsx
@@ -101,7 +101,7 @@ export default function WorkedOnSection({
           </Link>
         </div>
 
-        <div className="grid gap-5 sm:grid-cols-2 lg:grid-cols-4">
+        <div className="grid grid-cols-2 gap-3 sm:gap-5 lg:grid-cols-4">
           {projects.map((project) => (
             <a
               key={project.title}
